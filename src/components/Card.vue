5e9626b5f1ddcb8d034e218adc28b9abdfdb0cf3
Edward Stoner <edwardlstoner@gmail.com>
2020-05-18 21:17:07 -0600

    Add props and template

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,15 @@
-<template> </template>
+<template>
+  <div class="card">
+    <div class="title">{{ title }}</div>
+    <div class="description">{{ description }}</div>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "Card",
+  props: ["title", "description"]
+};
 </script>
 
 <style></style>
