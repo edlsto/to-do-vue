202793b6337d3342524b382b38324d8e288f72d5
Edward Stoner <edwardlstoner@gmail.com>
2020-05-18 21:16:58 -0600

    Fill out component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <div id="app">hi!</div>
+  <div id="app">
+    <h1>Remember this!</h1>
+    <Form />
+    <CardContainer :items="items" />
+  </div>
 </template>
 
 <script>
+import Form from "./components/Form.vue";
+import CardContainer from "./components/cardContainer.vue";
 export default {
+  components: {
+    Form,
+    CardContainer
+  },
   name: "app",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      items: [
+        { title: "go to store", description: "get some stuff" },
+        { title: "go somewhere else", description: "get some more stuff" }
+      ]
     };
   }
 };
