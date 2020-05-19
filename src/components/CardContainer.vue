ab17df45beb746cc15bc2945df09884df6983d17
Edward Stoner <edwardlstoner@gmail.com>
2020-05-18 21:17:22 -0600

    Create CardContainer component

---
+++ src/components/CardContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card-container">
+    <div class="card" v-for="item in items" :key="item.key">
+      <Card :title="item.title" :description="item.description" />
+    </div>
+  </div>
+</template>
+
+<script>
+console.log("hi");
+import Card from "./Card.vue";
+export default {
+  components: {
+    Card
+  },
+  name: "CardContainer",
+  props: ["items"]
+};
+</script>
+
+<style></style>
