2077ee474d710373cf859f94afd557afb1454952
Edward Stoner <edwardlstoner@gmail.com>
2020-07-09 20:29:44 -0600

    Create Nav component

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="nav-bar">Check <span class="nav-bar-highlight">Yo'</span> Self</h1>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
+
+<style>
+.nav-bar {
+  background: #1f1e3d;
+  color: white;
+  text-transform: uppercase;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 800;
+  padding: 0.3em;
+}
+
+.nav-bar-highlight {
+  color: #ffc211;
+}
+</style>
