440e83f56f975ad7f4bac3b5b6730fb695ba7b4e
Edward Stoner <edwardlstoner@gmail.com>
2020-07-10 10:43:45 -0600

    Add draft task

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="form">
     <div class="form-item">
-      <label class="form-label">Task Title</label> <input class="form-input" />
+      <label class="form-label">Task Title</label>
+      <input class="form-input" type="text" />
     </div>
+    <ul class="draft-task-list">
+      <div
+        class="draft-task-container"
+        v-for="(task, index) in tasks"
+        v-bind:key="index"
+      >
+        <img
+          src="../assets/delete.svg"
+          alt="Delete task"
+          class="draft-task-delete-icon"
+        />
+        <li class="draft-task">
+          {{ task }}
+        </li>
+      </div>
+    </ul>
     <div class="form-item">
-      <label class="form-label">Task Item</label> <input class="form-input" />
+      <label class="form-label">Task Item</label>
+      <div class="task-item-container">
+        <input class="form-input" v-model="newTask" type="text" />
+        <button class="add-task-btn" v-on:click="tasks.push(newTask)"></button>
+      </div>
     </div>
     <button class="form-btn">Make Task List</button>
     <button class="form-btn">Clear All</button>
+    <div class="line"></div>
+    <button class="form-btn">Filter By Urgency</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Form"
+  name: "Form",
+  data() {
+    return {
+      title: "",
+      tasks: [],
+      newTask: ""
+    };
+  }
 };
 </script>
 
@@ -54,5 +84,35 @@ export default {
 }
 .form-label {
   color: #c8d3d8;
+}
+.line {
+  height: 1px;
+  background-color: #1f1f3d;
+  margin: 1em 0;
+}
+.task-item-container {
+  display: flex;
+}
+.add-task-btn {
+  width: 3.5em;
+  background: #1f1f3d;
+  outline: none;
+  border: none;
+  margin-left: 0.2em;
+}
+.draft-task {
+  list-style: none;
+  color: #fff;
+}
+.draft-task-delete-icon {
+  height: 1em;
+  margin-right: 0.3em;
+}
+.draft-task-container {
+  display: flex;
+  align-items: center;
+}
+.draft-task-list {
+  margin-bottom: 1em;
 }
 </style>
