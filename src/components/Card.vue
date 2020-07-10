935d40f9034e81053f3d98fb13b2435e4871ed99
Edward Stoner <edwardlstoner@gmail.com>
2020-07-10 15:14:31 -0600

    Add checkboxes for items

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,28 +2,53 @@
   <div class="card">
     <div class="content">
       <div class="title">{{ title }}</div>
-      <div class="item-container">
-        <img class="checkbox" src="../assets/checkbox.svg" />
-        <div class="description">{{ tasks }}</div>
+      <div class="items-container">
+        <div
+          class="item-container"
+          v-for="(task, index) in tasks"
+          v-bind:key="index"
+        >
+          <img
+            :src="!completed ? completedImg : notCompletedImg"
+            class="checkbox"
+            v-on:click="toggleDone"
+          />
+          <div class="description">
+            {{ task.description }}
+          </div>
+        </div>
       </div>
-      <div class="buttons-row">
-        <div class="urgent-container">
-          <img class="icon" src="../assets/urgent.svg" alt="Urgent" />
-          <p class="icon-text">Urgent</p>
-        </div>
-        <div class="delete-container">
-          <img class="icon" src="../assets/delete.svg" alt="Delete" />
-          <p class="icon-text">Delete</p>
-        </div>
+    </div>
+    <div class="buttons-row">
+      <div class="urgent-container">
+        <img class="icon" src="../assets/urgent.svg" alt="Urgent" />
+        <p class="icon-text">Urgent</p>
+      </div>
+      <div class="delete-container">
+        <img class="icon" src="../assets/delete.svg" alt="Delete" />
+        <p class="icon-text">Delete</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import notCompleted from "../assets/checkbox-active.svg";
+import completed from "../assets/checkbox.svg";
 export default {
   name: "Card",
-  props: ["title", "tasks"]
+  props: ["title", "tasks"],
+  data() {
+    return {
+      completedImg: completed,
+      notCompletedImg: notCompleted
+    };
+  },
+  methods: {
+    toggleDone: function() {
+      this.completed = !this.completed;
+    }
+  }
 };
 </script>
 
@@ -32,6 +57,9 @@ export default {
   border: 1px solid #c7d3d8;
   padding: 0.2em;
   background-color: #fafdff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 .title {
   font-weight: bold;
@@ -42,12 +70,12 @@ export default {
   height: 1em;
   margin-right: 0.5em;
   margin-top: 0.25em;
+  cursor: pointer;
 }
 
 .item-container {
   display: flex;
-  border-top: 1px solid #c7d3d8;
-  border-bottom: 1px solid #c7d3d8;
+
   padding: 0.7em 1em;
 }
 
@@ -58,16 +86,21 @@ export default {
 .buttons-row {
   display: flex;
   justify-content: space-between;
-  margin: 0.5em 1em;
+  margin: 0.5em 1em 0.2em 1em;
 }
 
 .delete-container,
 .urgent-container {
   text-align: center;
   text-transform: uppercase;
+  cursor: pointer;
 }
 
 .icon-text {
   font-size: 0.7em;
+}
+.items-container {
+  border-top: 1px solid #c7d3d8;
+  border-bottom: 1px solid #c7d3d8;
 }
 </style>
