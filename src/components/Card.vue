4d27aa832aba219486438240bd65857ca9594514
Edward Stoner <edwardlstoner@gmail.com>
2020-07-09 21:42:02 -0600

    Style card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="card">
-    <div class="title">{{ title }}</div>
-    <div class="item-container">
-      <img class="checkbox" src="../assets/checkbox.svg" />
-      <div class="description">{{ description }}</div>
+    <div class="content">
+      <div class="title">{{ title }}</div>
+      <div class="item-container">
+        <img class="checkbox" src="../assets/checkbox.svg" />
+        <div class="description">{{ description }}</div>
+      </div>
+      <div class="buttons-row">
+        <div class="urgent-container">
+          <img class="icon" src="../assets/urgent.svg" alt="Urgent" />
+          <p class="icon-text">Urgent</p>
+        </div>
+        <div class="delete-container">
+          <img class="icon" src="../assets/delete.svg" alt="Delete" />
+          <p class="icon-text">Delete</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +48,27 @@ export default {
 .item-container {
   display: flex;
   border-top: 1px solid #c7d3d8;
+  border-bottom: 1px solid #c7d3d8;
   padding: 0.7em 1em;
+}
+
+.icon {
+  height: 1.6em;
+}
+
+.buttons-row {
+  display: flex;
+  justify-content: space-between;
+  margin: 0.5em 1em;
+}
+
+.delete-container,
+.urgent-container {
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.icon-text {
+  font-size: 0.7em;
 }
 </style>
